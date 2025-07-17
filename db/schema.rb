b6656d2ffd17a84c2ccfb417a7bea21c1dbbb2c8
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2025-07-17 20:22:35 -0300

    Create migration for books table and seed initial data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_17_185255) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_17_214924) do
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.string "genre"
+    t.string "isbn"
+    t.integer "total_copies"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "sessions", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "ip_address"
