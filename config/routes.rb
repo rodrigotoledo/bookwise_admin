0b55b32610600643ee64ead6f88931dd59407f05
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2025-07-17 18:40:39 -0300

    Add authentication concerns and dashboard controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root "dashboard#index"
+  resource :session
+  resource :registration, only: %i[new create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
