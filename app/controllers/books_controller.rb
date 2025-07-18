0d96e10a5f039f1a14d6310c0dc8c536274357d9
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2025-07-18 14:17:40 -0300

    Update before_action and add return_book and borrow methods

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class BooksController < ApplicationController
-  before_action :require_librarian, except: [ :index ]
-  before_action :set_book, only: %i[edit update destroy]
+  before_action :require_librarian, except: %i[index borrow]
+  before_action :set_book, only: %i[edit update destroy return_book borrow]
 
   def index
     @books = if params[:query].present?
@@ -42,12 +42,31 @@ class BooksController < ApplicationController
     redirect_to books_path, notice: "Book deleted."
   end
 
+  def return_book
+    user = User.find(params[:user_id])
+    if Borrowing.return_for(librarian: Current.user, user: user, book: @book)
+      redirect_to books_path, notice: "Book returned successfully."
+    else
+      redirect_to books_path, alert: "Could not return the book."
+    end
+  rescue
+    redirect_to books_path, alert: "Cant return a book without user."
+  end
+
+  def borrow
+    if @book.available_for?(Current.user) && @book.borrowings.create(user: Current.user)
+      redirect_to books_path, notice: "Book borrowed successfully"
+    else
+      redirect_to books_path, alert: "Ops... Cant borrow this Book"
+    end
+  end
+
   private
 
   def set_book
     @book = Book.find(params[:id])
   rescue
-  redirect_to books_path, alert: "Ops... Book not found"
+    redirect_to books_path, alert: "Ops... Book not found"
   end
 
   def book_params
